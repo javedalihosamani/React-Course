cb9e66b9c49d59daa6c7d794f78f801d452b2c04
javedalihosamani <javed.hosamani007@gmail.com>
2024-11-26 16:21:32 +0530

    React Styled Component is Completed

---
+++ 03-Styling-In-React/src/components/styled.component/StyledComponent.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { styles } from '../object-style/style'
+import styled from 'styled-components';
+import { StyledButton } from './Button/Button';
+
+const StyledComponent = () => {
+    const Title = styled.h1`
+        font-size: 1.5em;
+        text-align: center;
+        color: palevioletred;
+    `;
+  return (
+    <>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <div style={styles.Container}>
+                        <h2 style={styles.Header}>6) STYLED COMPONENT</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Title>Hello Styled Component</Title>
+                    <StyledButton>Styled Button</StyledButton>
+                    <StyledButton changeColor='tryIT'>Styled Button</StyledButton>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default StyledComponent
