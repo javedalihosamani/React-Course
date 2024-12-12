567d013a3596432aed9d3d37f9f6d705acf4f851
javedalihosamani <javed.hosamani007@gmail.com>
2024-12-12 15:31:49 +0530

    React Lifecycle Example is Completed

--- 06-Lifecycle/src/App.js
+++ 06-Lifecycle/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-import Counter from './components/Counter';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import Counter from "./components/Counter";
 
 export default class App extends Component {
   constructor(props) {
@@ -12,14 +12,18 @@ export default class App extends Component {
   }
 
   //User Defined Method
-  mount(){
-    const root = ReactDOM.createRoot(document.getElementById('renderHere'));
-    root.render(
+  mount() {
+    ReactDOM.createRoot(document.getElementById("renderHere")).render(
       <React.StrictMode>
-        <Counter />
+        <Counter counter={0} />
       </React.StrictMode>
     );
-  };
+  }
+
+  unmount() {
+    console.log("component UnMounted..!");
+    ReactDOM.createRoot(document.getElementById("renderHere")).render(null);
+  }
 
   render() {
     //console.log("THIS IS A PARENT COMPONENT RENDER METHOD");
@@ -37,16 +41,26 @@ export default class App extends Component {
           <div className="row">
             <div className="col">
               <div className="lead p-3 rounded border shadow-sm">
-                <h2 className='text-center'>{this.state.name}</h2>
+                <h2 className="text-center">{this.state.name}</h2>
                 <hr />
-                <button className="btn btn-outline-success" onClick={this.mount}>Mount</button>
-                <button className="btn btn-outline-danger float-end">UnMount</button>
+                <button
+                  className="btn btn-outline-success"
+                  onClick={this.mount}
+                >
+                  Mount
+                </button>
+                <button
+                  className="btn btn-outline-danger float-end"
+                  onClick={this.unmount}
+                >
+                  UnMount
+                </button>
               </div>
             </div>
           </div>
         </div>
         <section id="renderHere"></section>
       </Fragment>
-    )
+    );
   }
 }
--- 06-Lifecycle/src/components/Counter.jsx
+++ 06-Lifecycle/src/components/Counter.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
 
 export default class Counter extends Component {
     constructor(props){
@@ -10,14 +11,54 @@ export default class Counter extends Component {
         console.log("THIS IS A CHILD COMPONENT CONSTRUCTOR METHOD");
     }
 
-    componentWillMount(){
-        console.log("THIS IS A CHILD COMPONENT componentWillMount METHOD");
+    static getDerivedStateFromProps(props, state) {
+      console.log(`THIS IS A CHILD COMPONENT getDerivedStateFromProps METHOD`);
+      console.log(`PROPS VALUE IS : ${props.counter}`);
+      console.log(`STATE VALUE IS : ${state.count}`);
+      return true
     }
 
     componentDidMount(){
         console.log("THIS IS A CHILD COMPONENT componentDidMount METHOD");
     }
 
+    // User Defined Method
+    updateCounter(){
+      const root = ReactDOM.createRoot(document.getElementById('renderHere'));
+      root.render(
+        <React.StrictMode>
+          <Counter counter={this.props.counter+1}/>
+        </React.StrictMode>
+      );
+    }
+    
+    // User Defined Method
+    updateCount(){
+      this.setState({count: this.state.count + 1});
+    }
+
+    shouldComponentUpdate(props, state){
+      console.log(`THIS IS A CHILD COMPONENT shouldComponentUpdate METHOD`);
+      console.log(`CURRENT STATE VALUE IS : ${state.count}`);    
+      console.log(`CURRENT PROPS VALUE IS : ${props.counter}`);    
+      /* this.setState({isActive: !this.state.isActive })
+      console.log(`CURRENT STATE VALUE IS : ${this.state.isActive}`);  */  
+      return true;
+    }
+
+    getSnapshotBeforeUpdate(oldProps, oldState) { 
+      console.log(`THIS IS A CHILD COMPONENT getSnapshotBeforeUpdate METHOD`);     
+      console.log(`OLD PROPS VALUE IS : ${oldProps.counter}`);     
+      console.log(`OLD STATE VALUE IS : ${oldState.count}`);
+      return true; 
+      }
+
+    componentDidUpdate(props, state, snap){
+      console.log(`THIS IS A CHILD COMPONENT componentDidUpdate METHOD`); 
+      console.log(`PROPS VALUE IS : ${props.counter}`); 
+      console.log(`STATE VALUE IS : ${state.count}`); 
+      console.log(`SNAP VALUE IS : ${snap}`); 
+    }
     
   render() {
     console.log("THIS IS A CHILD COMPONENT RENDER METHOD");
@@ -30,7 +71,37 @@ export default class Counter extends Component {
                 </div>
             </div>
         </div>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <div className="lead p-3 border rounded text-center">
+                      <h1>PROPS VALUE IS : {this.props.counter}</h1>
+                      <h1>STATE VALUE IS : {this.state.count}</h1>
+                      {
+                        this.state.isActive ?
+                        <h2 className='text-success'>WELCOME TO LIFECYCLE METHODS</h2>
+                        : (
+                          <div>
+                            <h2>Need More Counts</h2>
+                            <button 
+                            className="btn btn-outline-warning"
+                            onClick={this.updateCounter.bind(this)}>Add +1 (Update Props value)</button>
+                            <button 
+                            className="btn btn-outline-warning"
+                            onClick={this.updateCount.bind(this)}>Add +1 (Update State value)</button>
+                          </div>
+                        )
+                      }
+                    </div>
+                </div>
+            </div>
+        </div>
       </Fragment>
     )
   }
+
+  /* componentWillUnmount(){
+    console.log("Parent Component UnMounted..!");
+  } */
 }
+
